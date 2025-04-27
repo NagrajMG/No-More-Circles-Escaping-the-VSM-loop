63b24fe681a356d13e13aa5a4d580bb76a7732fc
Nagraj Gaonkar <nagrajgaonkarmumbai749@gmail.com>
2025-04-27 23:35:45 +0530

    Initial commit

---
+++ Main_project_code/util.py
@@ -0,0 +1,46 @@
+# Add your import statements here
+import nltk
+import re
+import os
+import json
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+# import tensorflow as tf
+# from keras.models import load_model
+# from keras.preprocessing.text import Tokenizer
+# from tensorflow.keras.preprocessing.sequence import pad_sequences
+# from keras.layers import Dense, RNN, Embedding, Bidirectional, AbstractRNNCell
+from nltk.stem import WordNetLemmatizer
+from nltk.stem import PorterStemmer
+from nltk.tokenize import TreebankWordTokenizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from nltk.tokenize import PunktSentenceTokenizer
+from nltk.corpus import wordnet
+from nltk.corpus import stopwords
+nltk.download('averaged_perceptron_tagger_eng')
+nltk.download('wordnet')
+nltk.download('stopwords')
+nltk.download('punkt')
+# Add any utility functions here
+
+delimiter = r'[.?!]'
+subword_connectors = r"[-_/',]"
+whitespaces = r'\s+'
+
+# Function to map POS tags to WordNet POS tags
+def pos_mapping(tag):
+	if tag.startswith('J'): #if the token is an adjective
+		return wordnet.ADJ
+	elif tag.startswith('V'): #if the token is a verb
+		return wordnet.VERB
+	elif tag.startswith('N'): #if the token is a noun
+		return wordnet.NOUN
+	elif tag.startswith('R'): #if the token is an adverb
+		return wordnet.ADV
+	else:          
+		return None
+			
+# Function to remove stopwords from a list of tokens
+def stopwords_sieve(tokens , stopwords_list):
+	return [token for token in tokens if token.lower() not in stopwords_list]
